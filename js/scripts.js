f5153ea03fba953991a9ea81fa9b06c2d4ea7655
Abdulrahman Abutair <ab.abutair@gmail.com>
2021-05-03 17:08:23 +0200

    first deploy

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(function () {
+    var fullHeight = $(window).height();
+    var headerHeight = $('header nav').height();
+
+    $('.intro, .banner').css('margin-top', headerHeight);
+    $('.intro').css('height', fullHeight - headerHeight);
+})
+
+//--------------------------product-details-new-------------------
+$(function () {
+    var staticsHeight = $('.statics').height() + 30;
+    $('.reporting').css('height', staticsHeight)
+})
+
+$(function () {
+    var machineHeight = $('.customize-machine').height();
+    $('.the-machine').css('height', machineHeight)
+    $('.specifications').css('height', machineHeight)
+})
